d85c7185e01a3773114cf2e9e30b0e7647e90450
Герасимов Алексей <gerasimov.alexey1994@gmail.com>
2023-08-22 14:13:11 +0300

    added review service and few models

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,13 +1,24 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ReviewModel } from 'src/review/review.model';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { REVIEW_NOT_FOUND } from 'src/review/constants';
+import { CreateReviewDTO } from 'src/review/dto/create-review.dto';
+import { ReviewService } from 'src/review/review.service';
 
 @Controller('review')
 export class ReviewController {
+	constructor(private readonly reviewService: ReviewService) {}
 	@Post('create')
-	async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+	async create(@Body() dto: CreateReviewDTO) {
+		return this.reviewService.create(dto);
+	}
 
 	@Get(':id')
-	async get(@Param('id') id: string) {}
+	async get(@Param('id') id: string) {
+		const deletedDoc = await this.reviewService.delete(id);
+
+		if (!deletedDoc) {
+			throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
+	}
 
 	@Get('byProduct/:productId')
 	async getByProduct(@Param('productId') productId: string) {}
